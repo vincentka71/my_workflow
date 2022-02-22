4c1b46c80170d88fd0766c4d1b76c7e6ef08a9ec
vincent <vincent.kapp@gmail.com>
2022-02-22 15:19:40 +0100

    Erreur sur Zoompage remplacement png par svg

--- src/pages/zoompage/zoompage.component.jsx
+++ src/pages/zoompage/zoompage.component.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import './zoompage.styles.css';
+import "./zoompage.styles.css";
 import Header from "../../components/header/header.component";
 import Footer from "../../components/footer/footer.component";
 
-import workflowzoom from '../../assets/workflow_zoom.png'
-
-import {ReactComponent as ZoomOut} from '../../assets/zoomout.svg';
+import { ReactComponent as WorkflowZoom } from "../../assets/workflow_zoom.svg";
+import { ReactComponent as ZoomOut } from "../../assets/zoomout.svg";
 
 const ZoomPage = () => {
-    const navigate = useNavigate();
-    const routeChange = () =>{ 
-      let path = "generalpage"; 
-      navigate(`/${path}`);
-    }
-
-return (
-    <div className='generalpage'>
-    <Header title = {"Présentation du Workflow"} subtitle={"Vue générale"} backgroundcolor = {"#6AB9B4"} />
-    <img src={workflowzoom} alt='workflow-zoom' className='workflow-zoom'/>
-    { /* <WorkflowZoom className='workflow-zoom'/> */}
-    <button className='zoom-button' type='submit' onClick={routeChange}>
-      <ZoomOut className="zoomout" />
-    </button>
-    <Footer text = {"Présentation du Workflow - vue zoomée"} backgroundcolor = {"#6AB9B4"}/>
-    </div>
-)
-}
   
+  const navigate = useNavigate();
+  const routeChange = () => {
+    let path = "generalpage";
+    navigate(`/${path}`);
+  };
+
+  return (
+    <div className="generalpage">
+      <Header
+        title={"Présentation du Workflow"}
+        subtitle={"Vue générale"}
+        backgroundcolor={"#6AB9B4"}
+      />
+      <div className="content">
+        <WorkflowZoom className="workflow-zoom" />
+        <button className="zoom-button" type="submit" onClick={routeChange}>
+          <ZoomOut className="zoomout" />
+        </button>
+      </div>
+      <Footer
+        text={"Présentation du Workflow - vue zoomée"}
+        backgroundcolor={"#6AB9B4"}
+      />
+    </div>
+  );
+};
+
 export default ZoomPage;
